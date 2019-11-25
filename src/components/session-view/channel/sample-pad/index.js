1d88e31b8ab7c6b030ca438444ef94839920b3ab
dan jones <dannyjones360@gmail.com>
2019-11-25 23:03:47 +0000

    Replace audio package

--- src/components/session-view/channel/sample-pad/index.js
+++ src/components/session-view/channel/sample-pad/index.js
@@ -1,53 +1,43 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Pizzicato from 'pizzicato';
+import Wad from 'web-audio-daw';
 import isEqual from 'lodash/isEqual';
 
 import Store from '../../../../store';
 import mutateObject from '../../../../helpers/mutate-object';
 
-const useCompare = (val: any) => {
-    const prevVal = usePrevious(val)
-    return !isEqual(prevVal, val)
-}
-
-const usePrevious = (value) => {
-    const ref = useRef();
-    useEffect(() => {
-      ref.current = value;
-    });
-    return ref.current;
-}
+// const useCompare = (val: any) => {
+//     const prevVal = usePrevious(val)
+//     // console.log('prevVal', prevVal);
+//     // console.log('val', val);
+//     return !isEqual(prevVal, val)
+// }
+//
+// const usePrevious = (value) => {
+//     const ref = useRef();
+//     useEffect(() => {
+//       ref.current = value;
+//     });
+//     return ref.current;
+// }
 
 function Channel(props) {
-  const [inititalLoopFlag, setInititalLoopFlag] = useState(false);
-  let hasLoopsChanged = useCompare(props.loop.instances)
-
   const [sample, setSample] = useState(null);
   const [error, setError] = useState(false);
 
   const store = Store.useStore();
   const cueLoop = store.get('cueLoop');
   const settings = store.get('settings');
-  // will only fire once on initial mount
-  const useOnce = () => useEffect(() => {
-    hasLoopsChanged = true;
-    setInititalLoopFlag(true);
-  }, [])
 
-  useOnce();
 
   useEffect(() => {
-    const loadSample = new Pizzicato.Sound(props.file, function(error) {
-      if (error) {
-        setError(true);
-      } else {
-        console.log('render');
-        setSample(loadSample);
-        loadSample.on('play', handlePlay);
-
-      }
-    });
+    if (props.effects && props.effects.length) {
+      handleEffects();
+    } else {
+      const wadSample = new Wad({source : props.file})
+      setSample(wadSample);
+    }
   }, [props]);
+
 
   const handleEffects = () => {
     var i;
@@ -63,78 +53,24 @@ function Channel(props) {
         }
       }
 
-      const delay = new Pizzicato.Effects[effect.name](propertyArray);
-      sample.stop();
-      sample.removeEffect(delay);
-      sample.addEffect(delay);
+      const wadSample = new Wad(
+        {
+          source : props.file,
+          [effect.name] : propertyArray
+        }
+      )
+
+      setSample(wadSample);
     }
   };
-
-  const handleLooping = () => {
-    console.log('handleLooping');
-    if (hasLoopsChanged || inititalLoopFlag) {
-      console.log('handleLooping', props.loop);
-      console.log('cueLoop.loopTime', cueLoop.loopTime);
-
-      const executeOrder = () => {
-        for (let i = 0; i < props.loop.instances.length; i++) {
-          setTimeout(() => {
-            sample.stop();
-            sample.play();
-          }, props.loop.instances[i].time)
-        }
-      }
-
-      const relativeLoopTime = cueLoop.loopTime;
-      setInterval(() => {
-        executeOrder()
-      }, relativeLoopTime)
-
-    executeOrder();
-    setInititalLoopFlag(false);
-    }
-  }
 
   const handleClick = () => {
-
-    sample.stop();
+    // sample.stop();
     sample.play();
-
   };
 
-  const handlePlay = () => {
-    if (cueLoop.isLooping) {
-      // Set initial cue starting time
-      if (!props.loop.instances.length) {
-        cueLoop.loopTime = performance.now();
-      }
-
-      props.loop.instances.push({
-        time: performance.now() - cueLoop.loopTime,
-      })
-
-      // store.set('settings')(
-      //   mutateObject(
-      //     settings,
-      //     props.loop,
-      //     'instances',
-      //     props.loop.instances
-      //   )
-      // );
-      //
-      // store.set('cueLoop')(cueLoop);
-    }
-
-    console.log('settings', settings);
-  }
 
   if (sample) {
-    if (props.effects && props.effects.length) handleEffects();
-    // if (props.loopInstances && props.loopInstances.length) handleLooping();
-    if (!cueLoop.isLooping && cueLoop.loopTime) {
-      handleLooping();
-    }
-
     return (
       <div className="col-24  session-view__channel__pad">
         <div className="flex  flex-wrap  align-center  justify-center  h-100" onClick={handleClick}>
--- src/store/index.js
+++ src/store/index.js
@@ -34,15 +34,15 @@ export default createConnectedStore({
             effects: [
               {
                 _unique: 236472,
-                name: 'Delay',
+                name: 'delay',
                 test: 1,
                 properties: {
                   _unique: 236472,
-                  feedback: 0,
-                  time: 0,
-                  mix: 0
+                  delayTime: 0,
+                  wet: 0,
+                  feedback: 0
                 }
-              }
+              },
             ],
             loop: {
               _key: randomKey(),
@@ -50,64 +50,64 @@ export default createConnectedStore({
               instances: []
             }
           },
-          {
-            _key: randomKey(),
-            name: 'kick-2',
-            active: false,
-            file: 'kick-2.wav',
-            effects: [
-              {
-                _unique: 8564,
-                name: 'Delay',
-                test: 1,
-                properties: {
-                  _unique: 236472,
-                  feedback: 0.8,
-                  time: 0.22,
-                  mix: 0.75
-                }
-              }
-            ]
-          }
+          // {
+          //   _key: randomKey(),
+          //   name: 'kick-2',
+          //   active: false,
+          //   file: 'kick-2.wav',
+          //   effects: [
+          //     {
+          //       _unique: 8564,
+          //       name: 'Delay',
+          //       test: 1,
+          //       properties: {
+          //         _unique: 236472,
+          //         feedback: 0.8,
+          //         time: 0.22,
+          //         mix: 0.75
+          //       }
+          //     }
+          //   ]
+          // }
         ]
       },
-      {
-        _key: randomKey(),
-        name: 'snares',
-        color: '#000000',
-        samples: [
-          {
-            _key: randomKey(),
-            name: 'snare-1',
-            active: true,
-            file: 'https://alemangui.github.io/pizzicato/audio/wah.mp3',
-            effects: [
-              {
-                _unique: 236472,
-                name: 'Delay',
-                test: 1,
-                properties: {
-                  _unique: 236472,
-                  feedback: 0,
-                  time: 0,
-                  mix: 0
-                }
-              }
-            ],
-            loop: {
-              _key: randomKey(),
-              isActive: false,
-              instances: []
-            }
-          },
-          {
-            _key: randomKey(),
-            name: 'snare-2',
-            active: false,
-            file: 'snare-2.wav'
-          }
-        ]
-      }
+      // {
+      //   _key: randomKey(),
+      //   name: 'snares',
+      //   color: '#000000',
+      //   samples: [
+      //     {
+      //       _key: randomKey(),
+      //       name: 'snare-1',
+      //       active: true,
+      //       file: 'https://alemangui.github.io/pizzicato/audio/wah.mp3',
+      //       effects: [
+      //         {
+      //           _unique: 236472,
+      //           name: 'Delay',
+      //           test: 1,
+      //           properties: {
+      //             _unique: 236472,
+      //             feedback: 0,
+      //             time: 0,
+      //             mix: 0
+      //           }
+      //         }
+      //       ],
+      //       loop: {
+      //         _key: randomKey(),
+      //         isActive: false,
+      //         instances: []
+      //       }
+      //     },
+      //     {
+      //       _key: randomKey(),
+      //       name: 'snare-2',
+      //       active: false,
+      //       file: 'snare-2.wav'
+      //     }
+      //   ]
+      // }
     ]
   },
   cueLoop: {
